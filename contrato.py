19aa4b812c1650fea0b62aedacfe7b5d2592bfee
Otávio Augusto <101754873+atronee@users.noreply.github.com>
2022-10-14 08:10:32 -0300

    Create contrato.py

---
+++ contrato.py
@@ -0,0 +1,44 @@
+def cadastrar_ingrediente(tipo, ingrediente, valor):
+    """Cadastrar Ingrediente
+    :param tipo: tipo de ingrediente
+    :type tipo: string
+    :param ingrediente: ingrediente
+    :type ingrediemte: string
+    :param valor: valor do ingrediente
+    :type valor: float
+    """
+    pass
+
+def montar_pizzza_valor(massa, molho, queijo, cobertura):
+    """Montar pizza e retornar valor
+    :param massa: massa escolhida
+    :type massa: string
+    :param molho: molho escolhido
+    :type molho: string
+    :param queijo: queijo escolhido
+    :type queijo: string
+    :param cobertura: cobertura escolhida
+    :type cobertura: string
+    :return: preço da pizza
+    :return type: float
+
+    """
+    pass
+
+def remover_ingrediente(tipo,ingrediente):
+    """Remove ingrediente do inventário
+    :param tipo: tipo do ingrediente
+    :type tipo: string
+    :param ingrediente: ingrediente
+    :type ingrediente: string
+    """
+    pass
+
+def listar_ingredientes(tipo):
+    """Lista ingredientes e seus preços
+    :param tipo: tipo do ingrediente
+    :type tipo: string
+    :return: dicionário dos ingredientes desse tipo e seus respectivos preços
+    :return type: dict
+    """
+    pass
